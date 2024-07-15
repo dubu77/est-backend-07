20be1f80fe610fca6797f022c1952c3081557742
user <hyamster37@naver.com>
2024-07-16 07:25:02 +0900

    New

---
+++ src/main/java/com/example/estspringjuly/EstSpringJulyApplication.java
@@ -0,0 +1,13 @@
+package com.example.estspringjuly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstSpringJulyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstSpringJulyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/estspringjuly/EstSpringJulyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estspringjuly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstSpringJulyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
